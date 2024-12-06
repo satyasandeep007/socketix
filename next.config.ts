6a8b3d35739da5ba16a5beb0f3de1e9df1181795
Satya Sandeep <satyasandeep786@gmail.com>
2024-12-06 21:38:34 +0530

    feat: Initial Commit with twitter auth

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["pbs.twimg.com"],
+  },
+  /* config options here */
+};
+
+export default nextConfig;
